ad3626d5c58e2268d4bd6cc6fbd88d637aee43cc
Virucek <aykin.yakov@gmail.com>
2019-09-07 09:52:02 +0300

    modify hello class

--- hello.go
+++ hello.go
@@ -6,11 +6,13 @@ import (
 )
 
 func main() {
-	var n int
+	var n, a int
 	fmt.Scan(&n)
 	for i := 0; i < n; i++ {
 		fmt.Printf("hello, world\n")
 	}
-	duration := time.Duration(10)*time.Second
+	fmt.Println("Введите количество секунд для Sleep")
+	fmt.Scan(&a)
+	duration := time.Duration(a)*time.Second
 	time.Sleep(duration)
 }
